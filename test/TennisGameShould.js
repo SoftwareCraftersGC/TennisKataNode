3a84eebb76eb920197ee646b6a0f6d8ce37d9722
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-05-03 17:58:27 +0100

    Refactor: encapsulate score

--- test/TennisGameShould.js
+++ test/TennisGameShould.js
@@ -5,18 +5,16 @@ const should = require('chai').should();
 describe('A Tennis Game Should', () => {
 
     const game = Game();
-    const player_one = game.getFirstPlayer();
-    const player_two = game.getSecondPlayer();
 
     it('start with both players with zero points', () => {
-        player_one.score.should.be.equal(0);
-        player_two.score.should.be.equal(0);
+        game.scoreForFirstPlayer().should.be.equal(0);
+        game.scoreForSecondPlayer().should.be.equal(0);
     });
 
     it('add 15 points when a player scores for the first time', () => {
-        player_one.score.should.be.equal(0);
+        game.scoreForFirstPlayer().should.be.equal(0);
         game.incrementScoreForFirstPlayer();
-        player_one.score.should.be.equal(15);
+        game.scoreForFirstPlayer().should.be.equal(15);
     });
 })
 
@@ -24,17 +22,9 @@ function Game() {
     const playerOne = new Player();
     const playerTwo = new Player();
 
-    function getFirstPlayer() {
-        return playerOne;
-    }
-
-    function getSecondPlayer() {
-        return playerTwo;
-    }
-
     return {
-        getFirstPlayer : getFirstPlayer,
-        getSecondPlayer : getSecondPlayer,
+        scoreForFirstPlayer: () => playerOne.score,
+        scoreForSecondPlayer: () => playerTwo.score,
         incrementScoreForFirstPlayer: () => {
             playerOne.score += 15;
         }
